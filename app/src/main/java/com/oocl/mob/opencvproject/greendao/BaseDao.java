ff80ab6ad0707475fce8a2964dba783c18b78922
REX MAN (EUCD-EUC-ISD-OOCLL/ZHA) <rex.man@oocl.com>
2017-01-24 13:54:56 +0800

    add delete user,update user, queryuserlist function

--- app/src/main/java/com/oocl/mob/opencvproject/greendao/BaseDao.java
+++ app/src/main/java/com/oocl/mob/opencvproject/greendao/BaseDao.java
@@ -4,10 +4,12 @@ import com.oocl.mob.opencvproject.common.GreenDAOUtils;
 import com.oocl.mob.opencvproject.greendao.db.DaoSession;
 
 import org.greenrobot.greendao.AbstractDao;
+import org.greenrobot.greendao.query.QueryBuilder;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 /**
  * Created by manre on 1/19/17.
@@ -16,47 +18,103 @@ import java.lang.reflect.ParameterizedType;
 public abstract class BaseDao<T,K> {
 
 
-
-
-
-
+    /**
+     * insert entity t
+     * @param  t  need to save entity
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     * @throws NoSuchMethodException
+     */
     public void insert(T t) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Class tClazz  = getTClass();
+        K k=getDao();
+        Class kClazz=getKClass();
+        String functionNameForK="insert";
+        Method methodK=kClazz.getMethod(functionNameForK,tClazz);
+        methodK.invoke(k,t);
+    }
 
-//            Class tClazz  = getTClass();
-//            AbstractDao dao=GreenDAOUtils.getmInstance().getWritableDaoSession().getDao(tClazz);
-//            Class clazz=  dao.getClass();
-//            // get function name;
-//            String functionName="get"+tClazz.getSimpleName()+"Dao";
-//            Method method=clazz.getDeclaredMethod(functionName);
-//            method.invoke(dao,t);
+    /**
+     * query all data for this entity
+     * @return list of this entity
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     * @throws NoSuchMethodException
+     */
+    public List<T> queryList() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException
+    {
+        K k=getDao();
+        Class kClazz=getKClass();
+        String functionNameForK="queryBuilder";
+        Method methodK=kClazz.getMethod(functionNameForK);
+        QueryBuilder<T> qb= (QueryBuilder<T>) methodK.invoke(k);
+        List<T>list =qb.list();
+        return list;
+    }
 
+    /**
+     * get this Entity mapping dao class
+     *
+     * @return  T mapping's entity.
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
+    public K getDao() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         DaoSession daoSession=GreenDAOUtils.getmInstance().getWritableDaoSession();
         Class clazz=daoSession.getClass();
         Class tClazz  = getTClass();
         String functionNameForSession="get"+tClazz.getSimpleName()+"Dao";
         Method method=clazz.getDeclaredMethod(functionNameForSession);
         K k=(K)method.invoke(daoSession);
+        return k;
+    }
+
+    /**
+     * delete entity
+     * @param t  entity
+     * @throws NoSuchMethodException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    public void delete(T t) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Class tClazz  = getTClass();
+        K k=getDao();
         Class kClazz=getKClass();
-        String functionNameForK="insert";
-        Method methodK=kClazz.getDeclaredMethod(functionNameForK,tClazz);
+        String functionNameForK="delete";
+        Method methodK=kClazz.getMethod(functionNameForK,tClazz);
+        methodK.invoke(k,t);
+    }
+
+
+    public void update(T t)throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+
+        Class tClazz  = getTClass();
+        K k=getDao();
+        Class kClazz=getKClass();
+        String functionNameForK="update";
+        Method methodK=kClazz.getMethod(functionNameForK,tClazz);
         methodK.invoke(k,t);
     }
 
 
 
-
-
-
-
-
-
-
-    // Get T class
+    /**
+     * get T class entity
+     * @param <T>
+     * @return
+     */
     public  <T>  Class<T> getTClass() {
         return (Class<T>) ((ParameterizedType) getClass()
                 .getGenericSuperclass()).getActualTypeArguments()[0];
 
     }
+
+    /**
+     * get K class entity mapping class
+     * @param <K>
+     * @return
+     */
     public <K> Class<K> getKClass(){
         return (Class<K>)((ParameterizedType) getClass()
                 .getGenericSuperclass()).getActualTypeArguments()[1];
--- app/src/main/java/com/oocl/mob/opencvproject/greendao/UserDaoImpl.java
+++ app/src/main/java/com/oocl/mob/opencvproject/greendao/UserDaoImpl.java
@@ -20,26 +20,67 @@ public class UserDaoImpl extends BaseDao<User,UserDao> {
     public void insert(User user)
     {
 
-        DaoSession daoSession= GreenDAOUtils.getmInstance().getWritableDaoSession();
-        UserDao userDao=daoSession.getUserDao();
-        userDao.insert(user);
+//        DaoSession daoSession= GreenDAOUtils.getmInstance().getWritableDaoSession();
+//        UserDao userDao=daoSession.getUserDao();
+//        userDao.insert(user);
 
-//        try {
-//             super.insert(user);
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (NoSuchMethodException e) {
-//            e.printStackTrace();
-//        }
+        try {
+             super.insert(user);
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
     }
     public List<User> queryUserList(){
-        DaoSession daoSession= GreenDAOUtils.getmInstance().getWritableDaoSession();
-        UserDao userDao=daoSession.getUserDao();
-        QueryBuilder<User> qb=userDao.queryBuilder();
-        List<User> list=qb.list();
-        return list;
+//        DaoSession daoSession= GreenDAOUtils.getmInstance().getWritableDaoSession();
+//        UserDao userDao=daoSession.getUserDao();
+//        QueryBuilder<User> qb=userDao.queryBuilder();
+//        List<User> list=qb.list();
+//        return list;
+        List<User> list=null;
+        try {
+            list=  super.queryList();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+     return list;
+
+    }
+    public void deleteUser(User user)
+    {
+        try {
+//            UserDao userDao=super.getDao();
+//            userDao.delete(user);
+            super.delete(user);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    public void updateUser(User user)
+    {
+        UserDao userDao= null;
+        try {
+            userDao = super.getDao();
+            userDao.update(user);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
     }
 }
